5384db1cb23cf668f57d6c348332a1a1b5d923bf
Usman Developer <usman.attari002@gmail.com>
2020-10-05 13:49:39 +0500

    Fixed path.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/algo-visualization-prod/' : '/',
+    process.env.NODE_ENV === 'production' ? '/algo-visualization/' : '/',
   chainWebpack: config => {
     config.resolve.extensions
       .clear()

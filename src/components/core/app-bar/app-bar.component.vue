9767f4aeb2ca4fd8d49649bd6e04843925a38f1c
Usman Developer <usman.attari002@gmail.com>
2020-10-05 14:05:19 +0500

    .

--- src/components/core/app-bar/app-bar.component.vue
+++ src/components/core/app-bar/app-bar.component.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-dark bg-success">
     <router-link class="navbar-brand" to="/">
-      <img src="/images/logo.svg" width="40" />
+      <!-- <img src="/images/logo.svg" width="40" /> -->
       Algo Visualization
     </router-link>
   </nav>
